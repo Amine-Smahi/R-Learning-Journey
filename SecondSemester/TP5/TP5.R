ab009c9601c927205e7289e2c4b6faf3345d0a2c
Amine Smahi <mohammed.amine.smahi@gmail.com>
2019-07-09 19:40:58 +0100

    Add last tp about decision trees

---
+++ SecondSemester/TP5/TP5.R
@@ -0,0 +1,29 @@
+library(rpart)
+library(rpart.plot) 
+
+tit= read.csv2("titanic.csv",sep = ';')
+
+str(tit) 
+summary(tit)
+
+tit= na.omit(tit) 
+
+tit$Survived=factor(tit$Survived, levels = c(1, 0), labels = c("YES", "NO")) 
+ 
+tit$Pclass=factor(tit$Pclass, levels = c(1,2 ,3), labels = c("Upper", "Middle", "Lower"))
+
+indice=sample(nrow(tit), 100)
+training=tit[ indice, ] 
+test=tit[-indice, ]
+
+dim(traing)
+dim(test) 
+
+prop.table(table(training$Survived)) 
+
+ar=rpart(Survived~., data=traing, method="class") 
+rpart.plot(ar) 
+
+pred=predict(ar, test, type="class") 
+table_mat=table(test$Survived, pred) 
+accuracy= sum(diag(table_mat)) / sum(table_mat)
